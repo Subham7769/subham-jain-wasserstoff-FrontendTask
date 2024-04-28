b0b1a8a16c23dd2d9165f7c1061125e67d7d269b
Shubham Jain <shubham.rapariya2@gmail.com>
2024-04-28 14:30:49 +0530

    done

--- src/Components/FrontView/FrontView.jsx
+++ src/Components/FrontView/FrontView.jsx
@@ -1,43 +1,46 @@
-import React from 'react'
-import './FrontView.css'
-import logo from '../../Assets/Astrix Logo.png'
-import tile1 from '../../Assets/tile1.png'
-import tile2 from '../../Assets/tile2.png'
-import tile3 from '../../Assets/tile3.png'
-import tile4 from '../../Assets/tile4.png'
-import tile5 from '../../Assets/tile5.png'
-import tile6 from '../../Assets/tile6.png'
-import tile7 from '../../Assets/tile7.png'
-import tile8 from '../../Assets/tile8.png'
-
+import React from 'react';
+import './FrontView.css';
+import logo from '../../Assets/Astrix Logo.png';
+import tile1 from '../../Assets/tile1.png';
+import tile2 from '../../Assets/tile2.png';
+import tile3 from '../../Assets/tile3.png';
+import tile4 from '../../Assets/tile4.png';
+import tile5 from '../../Assets/tile5.png';
+import tile6 from '../../Assets/tile6.png';
+import tile7 from '../../Assets/tile7.png';
+import tile8 from '../../Assets/tile8.png';
 
 const Fronteview = () => {
-    return (
-        <div className='Fronteview'>
-            <div className='logoContainer'>
-                <img src={logo} alt="" />
-            </div>
-            <div className='Frontline'>
-                <span>
-                    we
-                    <img src={tile1} alt="" />
-                    ORGANIZE THE CONNECTION
-                    <img src={tile2} alt="" /><br/>
-                    <img src={tile3} alt="" />
-                    BETWEEN
-                    <img src={tile4} alt="" />
-                    MUSIC <br/>ARTIST
-                    <img src={tile5} alt="" />
-                    CULTURE
-                    <img src={tile6} alt="" /><br/>
-                    <img src={tile7} alt="" />
-                    ART
-                    <img src={tile8} alt="" />
-                    & COLLECTIONS
-                </span>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="Fronteview">
+      <div className="logoContainer">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="Frontline">
+        <span>
+          we
+          <img className='img' src={tile1} alt="Tile 1" />
+          ORGANIZE THE CONNECTION
+          <img className='img' src={tile2} alt="Tile 2" />
+          <br />
+          <img className='img' src={tile3} alt="Tile 3" />
+          BETWEEN
+          <img className='img2' src={tile4} alt="Tile 4" />
+          MUSIC
+          <br />
+          ARTIST
+          <img className='img' src={tile5} alt="Tile 5" />
+          CULTURE
+          <img className='img' src={tile6} alt="Tile 6" />
+          <br />
+          <img className='img' src={tile7} alt="Tile 7" />
+          ART
+          <img className='img' src={tile8} alt="Tile 8" />
+          & COLLECTIONS
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default Fronteview
+export default Fronteview;
