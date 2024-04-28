fa39c08d6ccf09508aa5081a47efc4ee3487df71
Shubham Jain <shubham.rapariya2@gmail.com>
2024-04-28 12:06:13 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import EventView from './Components/EventView/EventView';
 import FrontView from './Components/FrontView/FrontView';
 
+// Component that redirects on click
+const FrontViewRedirect = () => {
+  const navigate = useNavigate(); // Hook for navigation
+
+  const handleClick = () => {
+    navigate('/EventView'); // Navigate to /EventView when clicked
+  };
+
+  return (
+    <div onClick={handleClick} style={{ cursor: 'pointer' }}> {/* Change cursor to indicate clickable */}
+      <FrontView />
+    </div>
+  );
+};
+
 const App = () => {
   return (
-    <div className='container'>
-      <FrontView/>
-      {/* <EventView/> */}
-    </div>
-  )
-}
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<FrontViewRedirect />} /> {/* Clickable FrontView */}
+          <Route path="/EventView" element={<EventView />} /> {/* EventView component */}
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
