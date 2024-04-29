58ab55ddf9fceb6f960b347dc236ba43d521f6c1
Subham Jain <134865513+Subham7769@users.noreply.github.com>
2024-04-29 10:15:03 +0000

    done

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -2,15 +2,29 @@ import React, { useRef, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation, Controller } from "swiper/modules";
 import "swiper/css";
-import "swiper/css/pagination";
 import "swiper/css/navigation";
-import "swiper/css/controller"; // Ensure Controller CSS is imported
+import "swiper/css/controller";
 import './Slider.css';
 import image1 from '../../Assets/Image1.png';
 import image2 from '../../Assets/Image2.png';
 import image3 from '../../Assets/Image3.png';
 import image4 from '../../Assets/Image4.png';
-import location from '../../Assets/location_on.png';
+import locationIcon from '../../Assets/location_on.png';
+
+// Create a data structure for the slides to avoid repetition
+const mainSlides = [
+  { image: image1, eventName: "Event Name", location: "Location" },
+  { image: image2, eventName: "Event Name", location: "Location" },
+  { image: image3, eventName: "Event Name", location: "Location" },
+  { image: image4, eventName: "Event Name", location: "Location" },
+];
+
+const stripSlides = [
+  "Event: Oasis Bus tour, JLN Stadium, Delhi",
+  "Collection Live: Meta Lives, live on astrix",
+  "Event: Oasis Bus tour, JLN Stadium, Delhi",
+  "Collection Live: Meta Lives, live on astrix",
+];
 
 export default function Slider() {
   const mainSwiperRef = useRef(null);
@@ -23,16 +37,6 @@ export default function Slider() {
     }
   };
 
-  const handleMainSwiper = (swiper) => {
-    mainSwiperRef.current = swiper;
-    setControllers();
-  };
-
-  const handleStripSwiper = (swiper) => {
-    stripSwiperRef.current = swiper;
-    setControllers();
-  };
-
   useEffect(() => {
     return () => {
       if (mainSwiperRef.current) {
@@ -42,103 +46,68 @@ export default function Slider() {
         stripSwiperRef.current.controller.control = null;
       }
     };
-  }, []); 
+  }, []); // Cleanup controllers on unmount
 
   return (
-    <div className="slider-container"> 
+    <div className="slider-container">
       <div className="textContainer">
         <p>
-            ASTR<br/>IX
-            <br/>
-            EVE<br/>NTS
+          ASTR<br />
+          IX<br />
+          EVE<br />
+          NTS
         </p>
       </div>
-      
-      <div className="main-slider"> 
+
+      <div className="main-slider">
         <Swiper
-          onSwiper={handleMainSwiper}
+          ref={mainSwiperRef}
           slidesPerView={3}
           spaceBetween={20}
-          centeredSlides={true}
-          autoplay={{
-            delay: 1500,
-            disableOnInteraction: false,
+          centeredSlides
+          autoplay={{ delay: 1500, disableOnInteraction: false }}
+          onSwiper={(swiper) => {
+            mainSwiperRef.current = swiper;
+            setControllers();
           }}
           modules={[Autoplay, Navigation, Controller]}
           className="mySwiper"
         >
-          <SwiperSlide>
-            <img src={image1} alt="" />
-            <div className="slide">
-            <h1>Event Name</h1>
-            <div className="location">
-                <img src={location}  alt="" />
-                <p>Location</p>    
-            </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={image2} alt="" />
-            <div className="slide">
-            <h1>Event Name</h1>
-            <div className="location">
-                <img src={location}  alt="" />
-                <p>Location</p>    
-            </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={image3} alt="" />
-            <div className="slide">
-            <h1>Event Name</h1>
-            <div className="location">
-                <img src={location}  alt="" />
-                <p>Location</p>    
-            </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={image4} alt="" />
-            <div className="slide">
-            <h1>Event Name</h1>
-            <div className="location">
-                <img src={location}  alt="" />
-                <p>Location</p>    
-            </div>
-            </div>
-          </SwiperSlide>
+          {mainSlides.map((slide, index) => (
+            <SwiperSlide key={index}>
+              <img src={slide.image} alt={`Slide ${index + 1}`} />
+              <div className="slide">
+                <h1>{slide.eventName}</h1>
+                <div className="location">
+                  <img src={locationIcon} alt="Location" />
+                  <p>{slide.location}</p>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
-      
-      <div className="strip-slider"> 
+
+      <div className="strip-slider">
         <Swiper
-          onSwiper={handleStripSwiper}
+          ref={stripSwiperRef}
           slidesPerView={1}
           direction="vertical"
           spaceBetween={0}
+          onSwiper={(swiper) => {
+            stripSwiperRef.current = swiper;
+            setControllers();
+          }}
           modules={[Controller]}
           className="stripSwiper"
         >
-          <SwiperSlide>
-            <div className="strip">
-              <p>Event : Oasis Bus tour , JLN Stadium , Delhi </p>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="strip">
-              <p>Collection Live : Meta Lives , live on astrix</p>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="strip">
-              <p>vent : Oasis Bus tour , JLN Stadium , Delhi </p>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="strip">
-              <p>Collection Live : Meta Lives , live on astrix</p>
-            </div>
-          </SwiperSlide>
+          {stripSlides.map((text, index) => (
+            <SwiperSlide key={index}>
+              <div className="strip">
+                <p>{text}</p>
+              </div>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </div>
